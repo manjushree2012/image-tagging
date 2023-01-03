80b6731d9991c762eb9ab49de2da4eada5c77433
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2023-01-03 11:07:28 +0545

    added new route for Page1 view and a template view

---
+++ src/views/Page1View.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="form">
+        <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+                <input v-model="form.tag" class="input" type="text" placeholder = "Text here ...">
+            </div>
+        </div>
+    </section>
+  </template>
+
+<script>
+export default {
+  name: 'Page1View',
+  data() {
+    return {
+        form: {
+            tag: ''
+        }
+    }
+  },
+  components: {   
+  }
+}
+</script>
+
+
+  
