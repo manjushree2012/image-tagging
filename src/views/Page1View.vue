2b19802863d004c9e7f5b58fcdf6e0275a30b4d6
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2023-01-03 13:14:39 +0545

    implemented basic tags input

--- src/views/Page1View.vue
+++ src/views/Page1View.vue
@@ -1,26 +1,42 @@
 <template>
     <section class="form">
         <div class="field">
-            <label class="label">Name</label>
+            <label class="label">Enter tag here</label>
             <div class="control">
-                <input v-model="form.tag" class="input" type="text" placeholder = "Text here ...">
+                <input v-model="form.tag" class="input" type="text" placeholder = "Tag here ...">
             </div>
+        </div>
+
+        <div class="field">
+            <label class="label">Enter object key here</label>
+            <div class="control">
+                <input v-model="form.object_key" class="input" type="text" placeholder = "Object key here ...">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label">Enter keywords here</label>
+            <TagInput/>
         </div>
     </section>
   </template>
 
 <script>
+import TagInput from '@/components/TagInput.vue'
+
 export default {
   name: 'Page1View',
+  components: {
+    TagInput
+  },
   data() {
     return {
         form: {
-            tag: ''
+            tag: '',
+            object_key: ''
         }
     }
   },
-  components: {   
-  }
 }
 </script>
 
