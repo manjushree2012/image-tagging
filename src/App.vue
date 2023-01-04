aa6f87b2287187b3c423fa4a230aacb98746c72b
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2023-01-04 12:54:09 +0545

    added all necessary routes in template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/page1">Data Collection Interface</router-link> |
+    <router-link to="/filemanager">File Manager</router-link> |
+    <router-link to="/page2">ML Training Interface</router-link> |
   </nav>
   <router-view/>
 </template>
