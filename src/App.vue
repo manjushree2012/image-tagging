2df2b7ff08238013b69eea78d91c54446282d50b
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2023-01-04 12:56:36 +0545

    minor change: remove vue defaukt about page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
     <router-link to="/page1">Data Collection Interface</router-link> |
     <router-link to="/filemanager">File Manager</router-link> |
     <router-link to="/page2">ML Training Interface</router-link> |
