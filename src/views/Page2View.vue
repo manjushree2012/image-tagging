35e83f9b048ed69e21b054920cda9c1144b3c129
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2023-01-07 21:05:42 +0545

    minor design change: ML Training interface

--- src/views/Page2View.vue
+++ src/views/Page2View.vue
@@ -1,42 +1,62 @@
 <template>
     <div class="body">
         <form method="POST" @submit="checkForm">
-            <section class="form">
-                <div class="field">
+            <div class="form">
+
+                <div class="title">KU Image Tagging Project</div>
+                <div class="subtitle">ML Training Interface</div>
+
+                <div class="input-container ic1">
+                    <input v-model="form.object_key" id="object_key" class="input" type="text" placeholder=" " />
+                    <div class="cut"></div>
+                    <label for="object_key" class="placeholder">Object key here</label>
+                </div>
+
+                <!-- <div class="field">
                     <label class="label">Enter object key here</label>
                     <div class="control">
                         <input v-model="form.object_key" class="input" type="text" placeholder = "Object key here ...">
                     </div>
+                </div> -->
+
+                <div class="input-container ic2">
+                    <input v-model="form.source_key" id="source_key" class="input" type="text" placeholder=" " />
+                    <div class="cut"></div>
+                    <label for="source_key" class="placeholder">Source key here</label>
                 </div>
     
-                <div class="field">
+                <!-- <div class="field">
                     <label class="label">Enter source key here</label>
                     <div class="control">
                         <input v-model="form.source_key" class="input" type="text" placeholder = "Source key here ...">
                     </div>
+                </div> -->
+
+                <div class="input-container ic2">
+                    <input v-model="form.tag" id="tag" class="input" type="text" placeholder=" " />
+                    <div class="cut"></div>
+                    <label for="tag" class="placeholder">Tag here</label>
                 </div>
     
-                <div class="field">
+                <!-- <div class="field">
                     <label class="label">Enter tag here</label>
                     <div class="control">
                         <input v-model="form.tag" class="input" type="text" placeholder = "Tag here ...">
                     </div>
-                </div>
+                </div> -->
     
                 <div class="field is-grouped">
-                    <div class="control">
-                        <button class="button is-primary">Submit</button>
-                    </div>
+                <div class="control">
+                    <button class="button submit">Submit</button>
                 </div>
-            </section>
+            </div>
+            </div>
         </form>
     </div>
   </template>
 
 <script>
 import "@/assets/css/forms.css";
-
-
 import axios from 'axios';
 
 export default {
