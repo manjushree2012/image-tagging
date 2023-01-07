44426d6ae37889340f5299c1d62c4a313b36b23e
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2023-01-07 13:33:39 +0545

    page2: added css

--- src/views/Page2View.vue
+++ src/views/Page2View.vue
@@ -1,37 +1,42 @@
 <template>
-    <form method="POST" @submit="checkForm">
-        <section class="form">
-            <div class="field">
-                <label class="label">Enter object key here</label>
-                <div class="control">
-                    <input v-model="form.object_key" class="input" type="text" placeholder = "Object key here ...">
+    <div class="body">
+        <form method="POST" @submit="checkForm">
+            <section class="form">
+                <div class="field">
+                    <label class="label">Enter object key here</label>
+                    <div class="control">
+                        <input v-model="form.object_key" class="input" type="text" placeholder = "Object key here ...">
+                    </div>
                 </div>
-            </div>
-
-            <div class="field">
-                <label class="label">Enter source key here</label>
-                <div class="control">
-                    <input v-model="form.source_key" class="input" type="text" placeholder = "Source key here ...">
+    
+                <div class="field">
+                    <label class="label">Enter source key here</label>
+                    <div class="control">
+                        <input v-model="form.source_key" class="input" type="text" placeholder = "Source key here ...">
+                    </div>
                 </div>
-            </div>
-
-            <div class="field">
-                <label class="label">Enter tag here</label>
-                <div class="control">
-                    <input v-model="form.tag" class="input" type="text" placeholder = "Tag here ...">
+    
+                <div class="field">
+                    <label class="label">Enter tag here</label>
+                    <div class="control">
+                        <input v-model="form.tag" class="input" type="text" placeholder = "Tag here ...">
+                    </div>
                 </div>
-            </div>
-
-            <div class="field is-grouped">
-                <div class="control">
-                    <button class="button is-primary">Submit</button>
+    
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-primary">Submit</button>
+                    </div>
                 </div>
-            </div>
-        </section>
-    </form>
+            </section>
+        </form>
+    </div>
   </template>
 
 <script>
+import "@/assets/css/forms.css";
+
+
 import axios from 'axios';
 
 export default {
@@ -53,7 +58,7 @@ export default {
         e.preventDefault()
 
         axios
-        .get('http://localhost:8001/list')
+        .post('http://localhost:8001/autotag/ml/train/binary', this.form)
         .then(response => (this.files = response.data))
     }
 }
