42c7860402e26396b79fc57c8a7fb01e3086453e
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2023-01-28 15:48:07 +0545

    dataset key and key autofill

--- src/views/Page2View.vue
+++ src/views/Page2View.vue
@@ -6,23 +6,23 @@
                 <div class="title">KU Image Tagging Project</div>
                 <div class="subtitle">ML Training Interface</div>
 
-                <div class="input-container ic1">
-                    <input v-model="form.dataset_key" id="dataset_key" class="input" type="text" placeholder=" " />
-                    <div class="cut"></div>
-                    <label for="dataset_key" class="placeholder">Dataset key here</label>
-                </div>
-
-                <div class="input-container ic2">
-                    <input v-model="form.key" id="key" class="input" type="text" placeholder=" " />
-                    <div class="cut"></div>
-                    <label for="key" class="placeholder">Key here</label>
-                </div>
-
                 <div class="input-container ic2">
                     <input v-model="form.tag" id="tag" class="input" type="text" placeholder=" " />
                     <div class="cut"></div>
                     <label for="tag" class="placeholder">Tag here</label>
                 </div>
+
+                <div class="input-container ic1">
+                    <input v-model="dataset_key" id="dataset_key" class="input" type="text" placeholder=" " readonly />
+                    <div class="cut"></div>
+                    <label for="dataset_key" class="placeholder">Dataset key here</label>
+                </div>
+
+                <div class="input-container ic2">
+                    <input v-model="key" id="key" class="input" type="text" placeholder=" " readonly/>
+                    <div class="cut"></div>
+                    <label for="key" class="placeholder">Key here</label>
+                </div>               
     
                 <div class="field is-grouped">
                 <div class="control">
@@ -46,18 +46,34 @@ export default {
   data() {
     return {
         form: {
-            dataset_key: '',
-            key: '',
-            tag: ''
+            tag: '',
+            // dataset_key: '',
+            // key: '',
         }
+    }
+  },
+  computed: {
+    key() {
+        const suffix = '_model.zip'
+        return this.form.tag ? this.form.tag + suffix : null
+    },
+    dataset_key() {
+        const suffix = '_dataset.zip'
+        return this.form.tag ? this.form.tag + suffix : null
     }
   },
   methods: {
     checkForm: function(e) {
         e.preventDefault()
 
+        const data = {
+            tag : this.form.tag,
+            dataset_key : this.dataset_key,
+            key : this.key
+    }
+
         axios
-        .post('http://localhost/autotag/ml/train/binary', this.form)
+        .post('http://localhost/autotag/ml/train/binary', data)
         .then(
             axios.post('http://localhost/autotag/model/register', {
                 'template' : "keras/MultiClassSingleTagKerasStandardModelTemplateA.py",
